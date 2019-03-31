7707e95443e430756efce408e649d85002cfc3e7
kyle <kylemclellan@gmail.com>
2019-03-31 15:16:19 -0400

    added color

--- lib/interface.rb
+++ lib/interface.rb
@@ -1,4 +1,5 @@
 require 'pry'
+require 'colorize'
 require_relative 'scraper.rb'
 class CLI
 
@@ -9,10 +10,10 @@ class CLI
   end
 
   def run
-    puts "\tThere are currently #{@item_array.length} items in Warframe."
-    puts "\tIt's not your fault you can't remember them all..."
-    puts "\tTo find an item, please enter the first few letters."
-    puts "\tTo exit, type 'exit' at any time."
+    puts "\tThere are currently #{@item_array.length} items in Warframe.".cyan
+    puts "\tIt's not your fault you can't remember them all...".cyan
+    puts "\tTo find an item, please enter the first few letters.".cyan
+    puts "\tTo exit, type 'exit' at any time.".cyan
 
     input = gets.strip
 
@@ -24,12 +25,12 @@ class CLI
       end
 
       if display_array.length > 0
-        display_array.each_with_index{|item, index| puts "#{index + 1} - #{item.name}"}
+        display_array.each_with_index{|item, index| puts "#{index + 1} - #{item.name}".blue}
       else
-        puts "\tYour search had no results."
+        puts "\tYour search had no results.".red
       end
 
-      puts "\n\n\tEnter an item number for more information or a few letters for a new search."
+      puts "\n\n\tEnter an item number for more information or a few letters for a new search.".cyan
       input = gets.strip
 
       # Allows a user to continue to select items from the list
@@ -37,14 +38,14 @@ class CLI
 
       # Ensures the chosen number is within the range of the display array
         unless (1..display_array.length).to_a.include?(input.to_i)
-          puts "Please choose a number from the list."
+          puts "Please choose a number from the list.".red
           input = gets.strip
           next
         end
 
-        puts "\n\t#{display_array[input.to_i - 1].name}"
-        puts "\t#{Scraper.get_item_info(display_array[input.to_i - 1])}"
-        puts "\tEnter your next search or 'exit'."
+        puts "\n\t#{display_array[input.to_i - 1].name}".blue
+        puts "\n\t#{Scraper.get_item_info(display_array[input.to_i - 1])}".blue
+        puts "\tEnter your next search or 'exit'.".cyan
         input = gets.strip
       end
     end
